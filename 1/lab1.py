4a11db932e8ed56a4e9fbd138df2d66bb3b7989c
Sergey Bakaleynik <bilaserg@gmail.com>
2018-09-28 19:24:15 +0300

    upd. readme + add/up plotting

--- 1/lab1.py
+++ 1/lab1.py
@@ -3,10 +3,9 @@
 import argparse
 import re
 
-from numpy import *
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import random
+from numpy import *
 from scipy.special import gamma, gammainc
 
 
@@ -45,28 +44,28 @@ def p(t, *args):
     args = args[1:]
     if distribution_type == 'Exp':
         # Экспоненциальное распределение
-        return [exp(-args[0] * j) for j in t]
+        return array([exp(-args[0] * j) for j in t])
     elif distribution_type == 'U':
         # Равномерное распределение
-        return [1 if j < args[0] else \
-                    (args[1] - j) / (args[1] - args[0]) if args[0] <= j <= args[1] else \
-                        0 if j > args[1] else None for j in t]
+        return array([1 if j < args[0] else \
+                          (args[1] - j) / (args[1] - args[0]) if args[0] <= j <= args[1] else \
+                              0 if j > args[1] else None for j in t])
     elif distribution_type == 'Г':
         # Гамма распределение
-        return [1 - gammainc(args[0], j / args[1]) / gamma(args[0]) for j in t]
+        return array([1 - gammainc(args[0], j / args[1]) / gamma(args[0]) for j in t])
     elif distribution_type == 'TN':
         # Усеченное нормальное распределение
         c = 1 / (0.5 + math.erf(args[0] / args[1]))
-        return [c * (0.5 - math.erf((j - args[0]) / args[1])) for j in t]
+        return array([c * (0.5 - math.erf((j - args[0]) / args[1])) for j in t])
     elif distribution_type == 'R':
         # Релея распределение
-        return [exp(-args[0] * pow(j, 2)) for j in t]
+        return array([exp(-args[0] * pow(j, 2)) for j in t])
     elif distribution_type == 'W':
         # Вейбулла распределение
-        return [exp(-pow(j / args[1], args[0])) for j in t]
+        return array([exp(-pow(j / args[1], args[0])) for j in t])
     elif distribution_type == 'N':
         # Нормальное распределение
-        return [0.5 - math.erf((j - args[0]) / args[1]) for j in t]
+        return array([0.5 - math.erf((j - args[0]) / args[1]) for j in t])
     return None
 
 
@@ -75,28 +74,29 @@ def f(t, *args):
     args = args[1:]
     if distribution_type == 'Exp':
         # Экспоненциальное распределение
-        return [args[0] * exp(-args[0] * j) for j in t]
+        return array([args[0] * exp(-args[0] * j) for j in t])
     elif distribution_type == 'U':
         # Равномерное распределение
-        return [1 / (args[1] - args[0]) if args[0] <= j <= args[1] \
-                    else 0 if j < args[0] or j > args[1] \
-            else None for j in t]
+        return array([1 / (args[1] - args[0]) if args[0] <= j <= args[1] \
+                          else 0 if j < args[0] or j > args[1] \
+            else None for j in t])
     elif distribution_type == 'Г':
         # Гамма распределение
-        return [pow(j, args[0] - 1) / (pow(args[1], args[0]) * gamma(args[0])) * exp(-j / args[1]) for j in t]
+        return array([pow(j, args[0] - 1) / (pow(args[1], args[0]) * gamma(args[0])) * exp(-j / args[1]) for j in t])
     elif distribution_type == 'TN':
         # Усеченное нормальное распределение
         c = 1 / (0.5 + math.erf(args[0] / args[1]))
-        return [(c / (args[1] * sqrt(2 * pi))) * exp(-pow(j - args[0], 2) / (2 * pow(args[1], 2))) for j in t]
+        return array([(c / (args[1] * sqrt(2 * pi))) * exp(-pow(j - args[0], 2) / (2 * pow(args[1], 2))) for j in t])
     elif distribution_type == 'R':
         # Релея распределение
-        return [2 * args[0] * j * exp(-args[0] * pow(j, 2)) for j in t]
+        return array([2 * args[0] * j * exp(-args[0] * pow(j, 2)) for j in t])
     elif distribution_type == 'W':
         # Вейбулла распределение
-        return [((args[0] * pow(j, args[0] - 1)) / pow(args[1], args[0])) * exp(-pow(j / args[1], args[0])) for j in t]
+        return array(
+            [((args[0] * pow(j, args[0] - 1)) / pow(args[1], args[0])) * exp(-pow(j / args[1], args[0])) for j in t])
     elif distribution_type == 'N':
         # Нормальное распределение
-        return [1 / (args[1] * sqrt(2 * pi)) * exp(-pow(j - args[0], 2) / (2 * pow(args[1], 2))) for j in t]
+        return array([1 / (args[1] * sqrt(2 * pi)) * exp(-pow(j - args[0], 2) / (2 * pow(args[1], 2))) for j in t])
     return None
 
 
@@ -120,61 +120,54 @@ def parse_csv(variant=0):
     return None
 
 
+def plot(x, y, verbose):
+    dpi = 300
+    fig = plt.figure(dpi=dpi)
+    mpl.rcParams.update({'font.size': 10})
+    plt.axis([min(x), max(x), min(y), max(y)])
+    plt.title('')
+    plt.xlabel('t')
+    plt.ylabel(verbose)
+    plt.plot(x, y, color='blue', linewidth=1,
+             label=verbose)
+    plt.legend(loc='upper right')
+    fig.savefig('{}/{}.png'.format(verbose[0], verbose))
+    plt.close()
+
+
 parser = argparse.ArgumentParser(description='Обработка вариантов по ЛР1')
 parser.add_argument('-v', '--variant', type=int, default=0, help='Вариант')
 inputs = parser.parse_args()
 max_t, interval = 3000, 100
 objects = parse_csv(inputs.variant - 1)
 t = array([i for i in range(0, max_t + interval, interval)])
-P, F = [], []
 file = open("out.txt", "w", encoding='UTF-8')
+lc = 0
 for i, obj in enumerate(objects):
     moments = get_start_moment(*obj)
-    P += [p(t, *obj)]
-    F += [f(t, *obj)]
+    P = p(t, *obj)
+    F = f(t, *obj)
+    L = F / P
+    Q = 1 - P
+    lc += L
+    plot(t, P, "P_{}(t)".format(i + 1))
+    plot(t, F, "f_{}(t)".format(i + 1))
+    plot(t, L, "λ_{}(t)".format(i + 1))
+    plot(t, Q, "Q_{}(t)".format(i + 1))
     file.write("Объект {}".format(i + 1))
     file.write('\n')
-    file.write('\t'.join(["moments:", *["%.4f" % i for i in moments]]))
+    file.write('\t'.join(["moments:", *["%.4f" % j for j in moments]]))
     file.write('\n')
-    file.write('\t'.join(["t:", *["%.4f" % i for i in t]]))
+    file.write('\t'.join(["t:", *["%.4f" % j for j in t]]))
     file.write('\n')
-    file.write('\t'.join(["P:", *["%.4f" % i for i in P[-1]]]))
+    file.write('\t'.join(["P:", *["%.4f" % j for j in P]]))
     file.write('\n')
-    file.write('\t'.join(["F:", *["%.4f" % i for i in F[-1]]]))
+    file.write('\t'.join(["F:", *["%.4f" % j for j in F]]))
     file.write('\n')
-
-    dpi = 300
-    fig = plt.figure(dpi=dpi, figsize=(1024 / dpi, 768 / dpi))
-    mpl.rcParams.update({'font.size': 10})
-    plt.axis([0, max(t) + 1, min(min(P[-1]), min(F[-1])) - 0.1, max(max(P[-1]), max(F[-1])) + 0.1])
-    plt.title('')
-    plt.xlabel('t')
-    plt.ylabel('')
-    plt.plot(t, P[-1], color='blue', linewidth=1,
-             label='P_{}(t)'.format(i))
-    plt.plot(t, F[-1], color='red', linewidth=1,
-             label='F_{}(t)'.format(i))
-    plt.legend(loc='upper right')
-    fig.savefig('{}.png'.format(i))
-
-dpi = 300
-fig = plt.figure(dpi=dpi)
-mpl.rcParams.update({'font.size': 10})
-plt.axis([0, max(t) + 1, min(min(P[-1]), min(F[-1])) - 0.1, max(max(P[-1]), max(F[-1])) + 0.1])
-plt.title('')
-plt.xlabel('t')
-plt.ylabel('')
-colours = ["red", "green", "black", "yellow", "blue"]
-plt.axis([0, max(t) + 1, min([min(i) for i in P]) - 0.1, max([max(i) for i in P]) + 0.1])
-for i, val in enumerate(P):
-    plt.plot(t, val, color=colours[i], linewidth=1,
-             label='P_{}(t)'.format(i))
-plt.legend(loc='upper right')
-fig.savefig('P.png')
-fig.clear()
-plt.axis([0, max(t) + 1, min([min(i) for i in F]), max([max(i) for i in F])])
-for i, val in enumerate(F):
-    plt.plot(t, val, color=colours[i], linewidth=1,
-             label='F_{}(t)'.format(i))
-plt.legend(loc='upper right')
-fig.savefig('F.png')
+    file.write("\t".join(["λ:", *["%.4f" % j for j in L]]))
+    file.write('\n')
+    file.write("\t".join(["Q:", *["%.4f" % j for j in Q]]))
+    file.write('\n')
+plot(t, lc, "λ_с(t)")
+file.write("\t".join(["λ_с:", *["%.4f" % j for j in lc]]))
+file.close()
